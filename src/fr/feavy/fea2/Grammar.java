bdcecad3d0632b20c75e19235b56d41e0d774806
Feavy <quentin071100@live.fr>
2019-04-04 21:46:48 +0200

    Worked on new parsing system

--- src/fr/feavy/fea2/Grammar.java
+++ src/fr/feavy/fea2/Grammar.java
@@ -1,22 +1,145 @@
 package fr.feavy.fea2;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javafx.util.Pair;
 
 public class Grammar {
 
-	private Map<String, String> words;
+	private Map<String, String> symbols;
+	private Map<String, String> terminalSymbols;
+	
+	private List<String> keywords;
 	
 	public Grammar() {
-		this(new LinkedHashMap<String, String>());
+		this(new LinkedHashMap<String, String>(), new LinkedHashMap<String, String>());
 	}
 	
-	public Grammar(LinkedHashMap<String, String> words) {
-		this.words = words;
+	public Grammar(LinkedHashMap<String, String> symbols, LinkedHashMap<String, String> terminalSymbols) {
+		this.symbols = symbols;
+		this.terminalSymbols = terminalSymbols;
+		this.keywords = new ArrayList<String>();
+		autoExtractKeywords();
 	}
 	
-	public void putWord(String word, String regex) {
-		words.put(word, regex);
+	public void autoExtractKeywords() {
+		Pattern keywordPattern = Pattern.compile("[a-z_]+");
+		Matcher keywordMatcher;
+		
+		for(String regex : symbols.values()) {
+			keywordMatcher = keywordPattern.matcher(regex);
+			while(keywordMatcher.find()) {
+				String current = keywordMatcher.group();
+				if(!symbols.keySet().contains(current))
+					keywords.add(keywordMatcher.group());
+			}
+		}
+	}
+	public void putSymbol(String symbol, String regex) {
+		symbols.put(symbol, regex);
+	}
+	
+	public void putTerminalSymbol(String symbol, String regex) {
+		terminalSymbols.put(symbol.toUpperCase(), regex);
+	}
+	
+	public String toString() {
+		Iterator<Entry<String, String>> symbolsIterator = symbols.entrySet().iterator();
+		Iterator<Entry<String, String>> terminalSymbolsIterator = terminalSymbols.entrySet().iterator();
+		Entry<String, String> e;
+		
+		StringBuilder rep = new StringBuilder();
+
+		rep.append("-- Keywords --\n");
+		for(String keyword : keywords)
+			rep.append(keyword+"\n");
+		
+		rep.append("-- Symbols --\n");
+		while(symbolsIterator.hasNext()) {
+			e = symbolsIterator.next();
+			rep.append(e.getKey()+": "+e.getValue()+"\n");
+		}
+		
+		rep.append("-- Terminal Symbols --\n");
+		while(terminalSymbolsIterator.hasNext()) {
+			e = terminalSymbolsIterator.next();
+			rep.append(e.getKey()+": "+e.getValue()+"\n");
+		}
+		
+		return rep.toString();
+	}
+	
+	private String parseConstants(String input) {
+		Iterator<Entry<String, String>> terminalSymbolsIterator = terminalSymbols.entrySet().iterator();
+		Entry<String, String> e;
+		
+		
+		Matcher currentMatcher;
+		String current;
+		
+		while(terminalSymbolsIterator.hasNext()) {
+			e = terminalSymbolsIterator.next();
+			currentMatcher = Pattern.compile(e.getValue()).matcher(input);
+			while(currentMatcher.find()) {
+				current = currentMatcher.group();
+				if(!keywords.contains(current)) {
+					System.out.println("replace : "+current);
+					input = input.replaceFirst(current, e.getKey());
+					// Problème : remplace le premier 'fl' trouvé
+				}
+			}
+		}
+		System.out.println(input);
+		return input;
+	}
+	
+	private Pair<Boolean, String> parseCode(String input) {
+		boolean replaced = false;
+		String newCode = input;
+		replaced = false;
+		Matcher currentMatcher;
+		for(String word : symbols.keySet()) {
+			currentMatcher = Pattern.compile(symbols.get(word)).matcher(input);
+			while(currentMatcher.find()) {
+				newCode = newCode.replace(currentMatcher.group(), "<"+word+">");
+				replaced = true;
+				
+			}
+			input = newCode;
+		}
+		return new Pair<>(replaced, input);
+	}
+	
+	public boolean isValid(String input) {
+		Matcher currentMatcher;
+		
+		input = parseConstants(input);
+		
+		boolean replaced = false;
+		Pair<Boolean, String> parseReply;
+		
+		do {
+			parseReply = parseCode(input);
+			replaced = parseReply.getKey();
+			input = parseReply.getValue();
+			System.out.println(input);
+		}while(replaced);
+		
+		int length = 0;
+		
+		currentMatcher = Pattern.compile("<stmt>").matcher(input);
+		while(currentMatcher.find())
+			length += currentMatcher.end()-currentMatcher.start();
+		
+		System.out.println("Code final : "+input);
+		return input.length() == length;
 	}
 	
 }
--- src/fr/feavy/fea2/Main.java
+++ src/fr/feavy/fea2/Main.java
@@ -1,134 +1,71 @@
 package fr.feavy.fea2;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import javafx.util.Pair;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-		
-	public static Map<String, String> blocks = new LinkedHashMap<String, String>();
+
 	
-	public final static String KEY_WORDS = "if|else|while|string";
+	public static String block = "if(true){while{a:string=bonjour+test;a=autre;a=\"other\";nb:int=5;}}else{}while{}";
+
+	public static Grammar grammar = new Grammar();
 	
 	static {
-		blocks.put("<stmt>", "<compound_stmt>|<simple_stmt>");
+		grammar.putSymbol("stmt", "<compound_stmt>|<simple_stmt>");
 		
-		blocks.put("<compound_stmt>", "<if_stmt>|<while_stmt>");
-		blocks.put("<if_stmt>", "if\\{(<stmt>)*\\}(else\\{(<stmt>)*\\})?");
-		blocks.put("<while_stmt>", "while\\{((<stmt>)*)\\}");
+		grammar.putSymbol("compound_stmt", "<if_stmt>|<while_stmt>");
+		grammar.putSymbol("if_stmt", "if\\(<expr>\\)\\{(<stmt>)*\\}(else\\{(<stmt>)*\\})?");
+		grammar.putSymbol("while_stmt", "while\\{((<stmt>)*)\\}");
 		
-		blocks.put("<simple_stmt>", "(<var_assignment>|<var_declaration>|line);");
+		grammar.putSymbol("simple_stmt", "<var_assignment>|<var_declaration>;");
 		
-		blocks.put("<type>", "string|boolean");
-		blocks.put("<var_assignment_left>", "((<var_declaration>|NAME)=)");
-		blocks.put("<var_assignment>", "(<var_assignment_left><expr>)");
-		blocks.put("<var_declaration>", "(<expr>:<type>)");
-		blocks.put("<expr>", "(NAME|(<expr>(\\+<expr>)+))");
+		grammar.putSymbol("type", "string|boolean|int|float");
+		grammar.putSymbol("var_assignment_left", "(<var_declaration>|NAME)=");
+		grammar.putSymbol("var_assignment", "<var_assignment_left><expr>;");
+		grammar.putSymbol("var_declaration", "NAME:<type>|<var_declaration>\\[<expr>\\]");
+		
+		grammar.putSymbol("method_signature", "NAME\\((NAME:<type>(,NAME:<type>)*)?\\):<type>");
+		
+		grammar.putSymbol("expr", "true|false|NAME|STRING|NUMBER|(<expr>(\\+<expr>)+)");
+		
+		grammar.putTerminalSymbol("STRING", "\"[^\"]*\"");
+		grammar.putTerminalSymbol("NAME", "[a-zA-Z_][a-zA-Z0-9_]*");
+		grammar.putTerminalSymbol("NUMBER", "[0-9]+(\\.[0-9]+)?");
+		
+		grammar.autoExtractKeywords();
 	}
-	
-	public static Set<String> WORDS = blocks.keySet();
-	
-	public static String block = "if{while{a:string=bonjour+test;a=autre;}}else{}while{}";
-		
-	public static String method = "NAME(NAME, NAME(NAME, NAME))";
 	
 	public static void main(String[] args) {
-		
-		//blocks.replace("<var_declaration>", blocks.get("<var_declaration>").replaceAll("NAME", NAME));
-		//blocks.replace("<expr>", blocks.get("<expr>").replaceAll("NAME", NAME));
-		
-		
-		// 1 - Remplacer les strings par STRING, nombres par NUMBER...
-		// 2 - remplacer noms par NAME
-		// 3 - Exécuter la grammaire une fois
-		// 4 - Exécuter la grammaire en boucle tant que des <expr> sont transformés
-		// 5 - Exécuter la grammaire en boucle tant qu'il y a des transformations.
-		
-		Matcher currentMatcher;
 			
-		block = transformConstants(block);
-		block = parseCode(block).getValue();
-		block = parseExpressions(block);
-		
-		boolean replaced = false;
-		Pair<Boolean, String> parseReply;
-		
-		do {
-			parseReply = parseCode(block);
-			replaced = parseReply.getKey();
-			block = parseReply.getValue();
-			System.out.println(block);
-		}while(replaced);
-		
-		int length = 0;
-		
-		currentMatcher = Pattern.compile("<stmt>").matcher(block);
-		while(currentMatcher.find())
-			length += currentMatcher.end()-currentMatcher.start();
-		
-		System.out.println("Code final : "+block);
-		if(block.length() == length) {
-			System.out.println("==> Valide");
-		}else {
-			System.err.println("!!> Invalide");
+		try {
+			String code = loadCodeFromFile("/main.fea");
+			System.out.println(code);
+			System.out.println("Valide : "+grammar.isValid(code));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
-	}
-	
-	public final static String NAME = "[a-z][a-z0-9_]*";
-	public final static String STRING = "\"[^\"]*\"";
-	public final static String NUMBER = "[0-9]+(\\.[0-9]+)?";
-	
-	public static String transformConstants(String code) {
-		code = code.replaceAll(STRING, "STRING").replaceAll(NUMBER, "NUMBER");
-		Matcher nameMatcher = Pattern.compile(NAME).matcher(code);
-		String newCode = code;
-		String currentName;
-		while(nameMatcher.find()) {
-			currentName = code.substring(nameMatcher.start(), nameMatcher.end());
-			if(!currentName.matches(KEY_WORDS))
-				newCode = newCode.replaceFirst(currentName, "NAME");
-		}
-		return newCode;
-	}
-	
-	public static String parseExpressions(String code) {
-		String newCode = code;
-		boolean replaced = false;
-		String currentExpr;
-		do {
-			replaced = false;
-			Matcher exprMatcher = Pattern.compile(blocks.get("<expr>")).matcher(code);
-			while(exprMatcher.find()) {
-				currentExpr = code.substring(exprMatcher.start(), exprMatcher.end());
-				newCode = newCode.replace(currentExpr, "<expr>");
-				replaced = true;
-			}
-			code = newCode;
-		}while(replaced);
-		return code;
-	}
-	
-	public static Pair<Boolean, String> parseCode(String code) {
-		boolean replaced = false;
-		String newCode = code;
-		replaced = false;
-		Matcher currentMatcher;
-		for(String word : WORDS) {
-			currentMatcher = Pattern.compile(blocks.get(word)).matcher(newCode);
-			while(currentMatcher.find()) {
-				String c = block.substring(currentMatcher.start(), currentMatcher.end());
-				newCode = newCode.replace(c, word);
-				replaced = true;
-				
-			}
-			code = newCode;
-		}
-		return new Pair<>(replaced, code);
+		System.out.println(grammar.toString());
+		System.out.println();
+		System.out.println("Code valide : "+grammar.isValid(block));
+		
+		// 1 - Remplacer les strings par STRING, noms par NAME, nombres par NUMBER...
+		// 2 - Exécuter la grammaire en boucle tant qu'il y a des transformations.
 	}
 
+	public static String loadCodeFromFile(String path) throws IOException {
+		StringBuilder code = new StringBuilder();
+		String line;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(Main.class.getResourceAsStream(path)));
+		while((line = reader.readLine()) != null) {
+			line = line.replaceAll("\\s", "");
+			if(!line.startsWith("//"))
+				code.append(line);
+		}
+		reader.close();
+		return code.toString().replaceAll("/\\**.\\*/", "");
+	}
+	
 }
